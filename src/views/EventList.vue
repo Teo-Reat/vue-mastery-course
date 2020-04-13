293395f63e972a84f2b163ccf44bce839b5797ab
Teo Reat <teo.reat@gmail.com>
2020-04-13 17:15:27 +0300

    First commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Events Listing</h1>
+        <app-event-card/>
+    </div>
+</template>
+
+<script>
+    import EventCard from "@/components/EventCard";
+    export default {
+        components: {
+            appEventCard: EventCard
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Showing event #{{ id }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['id']
+    }
+</script>
+
+<style scoped>
+
+</style>
