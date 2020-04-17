c04d356886e9febb19c9d8e96d28c5a2ec9f747f
Teo Reat <teo.reat@gmail.com>
2020-04-17 17:17:29 +0300

    Vuex state

--- src/store.js
+++ src/store.js
@@ -4,7 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+    state: {
+        user: { id: 'abc123', name: 'Adam Jahr' },
+        categories: [
+            'Desire me sailor, ye addled sea!',
+            'All those ellipses will be lost in understandings like pressures in beauties',
+            'Lively, wet jolly rogers fast view a salty, dark lass.',
+            'All children like cut cauliflowers in maple syrup and sugar.',
+            'Hardened, muddy pudding is best decorated with large iced tea.',
+            'The misty cannon greedily hauls the ale.'
+        ]
+    },
+    mutations: {},
+    actions: {}
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Create Event</h1>
+    <div>
+        <h1>Create Event, {{ user.name }}</h1>
+        <p>User ID: {{ user.id }}</p>
+        <ul>
+            <li v-for="category in categories" :key="category">
+                <p>{{ category }}</p>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script>
+    import { mapState } from 'vuex'
+
+    export default {
+        computed: mapState(['user', 'categories']
+            // user: 'user',
+            // userID: state => state.user.id,
+            // categories: 'categories' //state => state.categories
+        )
+    }
+</script>
