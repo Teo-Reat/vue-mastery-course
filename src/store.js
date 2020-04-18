a49ca0c77efc4d8ce40579ffa16edb78ea412ba6
Teo Reat <teo.reat@gmail.com>
2020-04-18 10:46:01 +0300

    Vues getters

--- src/store.js
+++ src/store.js
@@ -13,8 +13,29 @@ export default new Vuex.Store({
             'All children like cut cauliflowers in maple syrup and sugar.',
             'Hardened, muddy pudding is best decorated with large iced tea.',
             'The misty cannon greedily hauls the ale.'
+        ],
+        todos: [
+            { id: 1, text: 'Gar, evil wave. go to la marsa beach.', done: false },
+            { id: 2, text: 'The tuna sails with life, burn the lighthouse until it waves.', done: true },
+            { id: 3, text: 'Planks travel on hunger at port degas!', done: false },
+            { id: 4, text: 'Damn yer gull, feed the mate.', done: true },
+            { id: 5, text: 'Rob me mate, ye old sail.', done: true },
         ]
     },
     mutations: {},
-    actions: {}
+    actions: {},
+    getters: {
+        categoriesLength: state => {
+            return state.categories.length
+        },
+        doneTodos: state => {
+            return state.todos.filter(todo => todo.done)
+        },
+        activeTodosCount: state => {
+            return state.todos.filter(todo => !todo.done).length
+        },
+        getEvent: state => id => {
+            return state.todos.find(event => event.id === id)
+        }
+    }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,22 +2,43 @@
     <div>
         <h1>Create Event, {{ user.name }}</h1>
         <p>User ID: {{ user.id }}</p>
+        <p>There are {{ categoriesLength }} categories.</p>
         <ul>
             <li v-for="category in categories" :key="category">
                 <p>{{ category }}</p>
             </li>
         </ul>
+        <hr>
+        <ul>
+            <li v-for="todo in todos" :key="todo">
+                <p>{{ todo }}</p>
+            </li>
+        </ul>
+        <p>{{ doneTodos }}</p>
+        <h2>{{ activeTodosCount }}</h2>
+        <p>{{ getEvent(1) }}</p>
     </div>
 </template>
 
 <script>
-    import { mapState } from 'vuex'
+    import { mapState, mapGetters } from 'vuex'
 
     export default {
-        computed: mapState(['user', 'categories']
-            // user: 'user',
-            // userID: state => state.user.id,
-            // categories: 'categories' //state => state.categories
-        )
+        computed: {
+            categoriesLength() {
+                return this.$store.getters.categoriesLength
+            },
+            // doneTodos() {
+            //     return this.$store.getters.doneTodos
+            // },
+            // activeTodos() {
+            //     return this.$store.getters.activeTodosCount
+            // },
+            // getEvent() {
+            //     return this.$store.getters.getEvent
+            // },
+            ...mapGetters(['doneTodos', "activeTodosCount", 'getEvent']),
+            ...mapState(['user', 'categories', 'todos'])
+        }
     }
 </script>
